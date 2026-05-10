e8a67a5a275e989cad98c263b05b29e7e8266ebb
Zachary Jones <jones_zachary@outlook.com>
2026-05-10 10:32:34 -0500

    update compatability date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
   css: ['~/assets/css/w3.css'],
+  compatibilityDate: '2026-05-10',
 
   devtools: {
     enabled: true,
