d0fce0522fd4ce1e7d571b8e7d33e026574b0fcf
Zachary Jones <jones_zachary@outlook.com>
2026-05-10 10:26:19 -0500

    refactor: update AppHeader component and enhance index page content

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,13 +1,10 @@
 <template>
   <div>
-    
+    <div class="w3-bar w3-yellow">
+      <a href="#" class="w3-bar-item w3-button">Home</a>
+      <a href="#" class="w3-bar-item w3-button">Link 1</a>
+      <a href="#" class="w3-bar-item w3-button">Link 2</a>
+      <a href="#" class="w3-bar-item w3-button">Link 3</a>
+    </div>
   </div>
 </template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
-</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <h1>Hello, World!</h1>
+    <h1>Welcome to TurboToad.net!</h1>
+    <p>This is my personal website.<br>I hope to put my projects here.</p>
   </div>
 </template>
 
-<style>
-
+<style scoped>
+div {
+  padding-left: 20px;
+}
 </style>
