4eb9e452bd292ff6bea57b04f4cb5c275e844338
Zachary Jones <jones_zachary@outlook.com>
2026-05-10 10:27:03 -0500

    Remove unused link entrys.

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <div class="w3-bar w3-yellow">
-      <a href="#" class="w3-bar-item w3-button">Home</a>
-      <a href="#" class="w3-bar-item w3-button">Link 1</a>
-      <a href="#" class="w3-bar-item w3-button">Link 2</a>
-      <a href="#" class="w3-bar-item w3-button">Link 3</a>
+      <a href="/" class="w3-bar-item w3-button">Home</a>
     </div>
   </div>
 </template>
